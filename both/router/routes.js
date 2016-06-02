a252bd5cd17215f8af904acf87d39cd18832ebad
jhmacdon <mcdonaldjosh7@gmail.com>
2016-06-01 22:07:17 -0700

    added an api endpoint

--- both/router/routes.js
+++ both/router/routes.js
@@ -20,3 +20,75 @@ Router.route("/:id/add", {
 Router.plugin('ensureSignedIn', {
   only: ['dashboard']
 });
+
+Router.route('/api/:id/balances', function () {
+  var req = this.request;
+  var res = this.response;
+  var id = req.originalUrl.split("/")[2]
+  group = Groups.findOne({_id:id})
+
+
+  records = Records.find({group:id});
+  if(!records.count() || records.count == 0) return false
+  data = {}
+  records.forEach(function(record){
+    index = record.user1._id + ":" + record.user2._id
+    if(record.type == "owes"){
+      //user1 owes user2
+      if(data[index]){
+        data[index].amount += Number(record.amount)
+      } else {
+        data[index] = {}
+        data[index].amount = Number(record.amount)
+        data[index].user1 = record.user1
+        data[index].user2 = record.user2
+      }
+    } else {
+      if(data[index]){
+        data[index].amount -= Number(record.amount)
+      } else {
+        data[index] = {}
+        data[index].amount = -Number(record.amount)
+        data[index].user1 = record.user1
+        data[index].user2 = record.user2
+      }
+    }
+  })
+
+  for (var key in data){
+    if (typeof data[key] !== 'function') {
+      value = data[key]
+      user = key.split(":")
+      if(data[user[1]+":"+user[0]]){
+        if(data[user[1]+":"+user[0]].amount < value.amount){
+          data[key].amount -= Number(data[user[1]+":"+user[0]].amount)
+          delete data[user[1]+":"+user[0]]
+        } else if (data[user[1]+":"+user[0]].amount > value.amount) {
+          data[user[1]+":"+user[0]].amount -= Number(data[key])
+          delete data[key]
+        }
+      }
+    }
+  }
+
+  /*var array = $.map(data, function(value, index) {
+    return [value];
+  });*/
+
+  var result = ""
+
+  for(var balance in data){
+    if(typeof data[balance] !== 'function'){
+      value = data[balance]
+
+      result += value.user1.profile.name + " owes " + value.user2.profile.name + " " + value.amount + ". "
+
+    }
+  }
+
+  res.end(result)
+
+
+
+  //res.end(Groups.findOne({_id:id}).toString());
+}, {where: 'server'});
